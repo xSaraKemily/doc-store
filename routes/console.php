0592a5b0e65c362f162ec7db0f4e15ddaff15542
saraKemily <sarakemili@hotmail.com>
2025-03-10 20:02:42 -0300

    fix: adjusted command to run daily

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:delete-old-files-command')->everyMinute(); //TODO: alterar pra daily
+Schedule::command('app:delete-old-files-command')->daily();
