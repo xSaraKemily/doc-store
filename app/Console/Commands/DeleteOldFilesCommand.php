95bb7aebafa5ef202246a572f0babd18f31cf66a
saraKemily <sarakemili@hotmail.com>
2025-03-10 16:18:35 -0300

    Add laravel-amqp package

--- app/Console/Commands/DeleteOldFilesCommand.php
+++ app/Console/Commands/DeleteOldFilesCommand.php
@@ -5,9 +5,8 @@ namespace App\Console\Commands;
 use App\Actions\DeleteFilesAction;
 use App\Models\File;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
 
-class DeleteFilesCommand extends Command
+class DeleteOldFilesCommand extends Command
 {
     protected $signature = 'app:delete-files-command';
 
