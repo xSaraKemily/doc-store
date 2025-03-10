e6aba1cc9c0c45df451814fc5e02e11d77f51db1
saraKemily <sarakemili@hotmail.com>
2025-03-10 19:54:32 -0300

    feat: added download option and first-reg img

--- resources/views/pages/file-list.blade.php
+++ resources/views/pages/file-list.blade.php
@@ -1,29 +1,56 @@
+@php use Illuminate\Support\Str; @endphp
 @extends('app')
 
 @section('content')
-    <div class="container mt-5">
-        <h2 class="text-center mb-4">Uploaded Files</h2>
+    <div class="container mt-5 d-flex flex-column justify-content-center align-items-center">
+        @if($files->isNotEmpty())
+            <h2 class="text-center mb-4">Uploaded Files</h2>
 
-        <table class="table table-striped">
-            <thead class="table-dark">
-            <tr>
-                <th scope="col">File Name</th>
-                <th scope="col">Upload Time</th>
-                <th scope="col">Action</th>
-            </tr>
-            </thead>
-            <tbody>
+            <table class="table table-striped">
+                <thead class="table-dark">
+                <tr>
+                    <th scope="col">File Name</th>
+                    <th scope="col">Upload Time</th>
+                    <th class="text-center" scope="col">Actions</th>
+                </tr>
+                </thead>
+
+                <tbody>
                 @foreach($files as $file)
                     <tr>
-                        <td class="col-6" title="{{$file->filename}}">{{ Str::limit($file->filename, 70, '...') }}</td>
+                        <td class="col-6" title="{{$file->filename}}">
+                            <div class="d-flex flex-row align-items-center gap-1">
+                                <img src="{{Str::endsWith($file->filename, '.docx') ? 'docx-icon.svg' : 'pdf-icon.svg'}}"
+                                     style="width: 30px;">
+
+                                {{ Str::limit($file->filename, 50, '...') }}
+                            </div>
+                        </td>
+
                         <td class="col-4">{{ $file->created_at->format('m/d/Y') }}</td>
+
                         <td>
-                            <button class="btn btn-danger open-delete-modal-button" data-bs-toggle="modal" data-bs-target="#deleteModal" data-file-id="{{ $file->id }}">Delete</button>
+                            <div class="d-flex flex-row align-items-center gap-1 justify-content-center">
+                                <button class="btn btn-danger open-delete-modal-button" data-bs-toggle="modal"
+                                        data-bs-target="#deleteModal" data-file-id="{{ $file->id }}">
+                                    <img src="trash.svg" style="width: 23px">
+                                </button>
+
+                                <a href="/download/{{ $file->id }}">
+                                    <button class="btn btn-info text-white">
+                                        <img src="download-icon.svg" style="width: 20px">
+                                    </button>
+                                </a>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        @else
+            <h2>There are no files uploaded yet</h2>
+            <img class="w-50" src="undraw_add-notes_9xls.svg">
+        @endif
     </div>
 
     <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
